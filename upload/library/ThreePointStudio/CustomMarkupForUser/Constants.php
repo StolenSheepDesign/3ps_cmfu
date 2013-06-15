29704067da6f7b0658eab3a959102e1ec37f80fd
tyteen4a03 <tyteen4a03@3pointstudio.com>
2013-06-16 03:30:38 +0800

    Code cleanup

--- upload/library/ThreePointStudio/CustomMarkupForUser/Constants.php
+++ upload/library/ThreePointStudio/CustomMarkupForUser/Constants.php
@@ -1,7 +1,11 @@
 <?php
+/*
+* Custom Username Markup For User v1.0.0 written by tyteen4a03@3.studIo.
+* This software is licensed under the BSD 2-Clause modified License.
+* See the LICENSE file within the package for details.
+*/
 
 class ThreePointStudio_CustomMarkupForUser_Constants {
-
 	public static $availableMarkups = array(
 		// Text Effects
 		"bold" => array(
