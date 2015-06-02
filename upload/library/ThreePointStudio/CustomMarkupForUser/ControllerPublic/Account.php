0fb577f39c9c2f7e73cdac78f610d071e1551477
Timothy Choi <tyteen4a03@3pointstudio.com>
2015-06-03 05:00:45 +0800

    Bump version to 1.1.0 Beta 1

--- upload/library/ThreePointStudio/CustomMarkupForUser/ControllerPublic/Account.php
+++ upload/library/ThreePointStudio/CustomMarkupForUser/ControllerPublic/Account.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Custom Username Markup For User v1.0.0 written by tyteen4a03@3.studIo.
+* Custom Markup For User v1.1.0 written by tyteen4a03@3.studIo.
 * This software is licensed under the BSD 2-Clause modified License.
 * See the LICENSE file within the package for details.
 */
