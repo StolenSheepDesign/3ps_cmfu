313bb085b225e4b0bbd31bee295fcbd57828779d
Timothy Choi <tyteen4a03@3pointstudio.com>
2015-06-05 02:44:14 +0800

    Fixed two installation/uninstallation bugs

--- upload/library/ThreePointStudio/CustomMarkupForUser/Installer.php
+++ upload/library/ThreePointStudio/CustomMarkupForUser/Installer.php
@@ -13,7 +13,7 @@ class ThreePointStudio_CustomMarkupForUser_Installer {
             $db->query("ALTER TABLE `xf_user`
                         ADD COLUMN  `3ps_cmfu_options` BLOB NULL AFTER `is_staff`");
             $db->query("CREATE TABLE IF NOT EXISTS `xf_3ps_cmfu_presets` (
-                  `preset_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                  `preset_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
                   `title` text COLLATE utf8_unicode_ci NOT NULL,
                   `enable_for` text COLLATE utf8_unicode_ci NOT NULL,
                   `display_style_priority` int(10) unsigned NOT NULL,
@@ -22,7 +22,6 @@ class ThreePointStudio_CustomMarkupForUser_Installer {
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
             ");
             $db->query("ALTER TABLE `xf_3ps_cmfu_presets`
-                ADD PRIMARY KEY (`preset_id`),
                 ADD UNIQUE KEY `preset_id` (`preset_id`);
             ");
             $db->query("ALTER TABLE `xf_data_registry` CHANGE `data_key` `data_key` VARBINARY(50) NOT NULL");
@@ -40,9 +39,11 @@ class ThreePointStudio_CustomMarkupForUser_Installer {
             }
 
             if ($version < 5) { // 1.0.1 - 1.1.0
-                $db->query("ALTER TABLE `xf_user` DROP COLUMN `3ps_cmfu_render_cache`");
+                try {
+                    $db->query("ALTER TABLE `xf_user` DROP COLUMN `3ps_cmfu_render_cache`");
+                } catch (Zend_Db_Statement_Mysqli_Exception $e) {}
                 $db->query("CREATE TABLE IF NOT EXISTS `xf_3ps_cmfu_presets` (
-                    `preset_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                    `preset_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
                     `title` text COLLATE utf8_unicode_ci NOT NULL,
                     `enable_for` text COLLATE utf8_unicode_ci NOT NULL,
                     `display_style_priority` int(10) unsigned NOT NULL,
@@ -51,7 +52,6 @@ class ThreePointStudio_CustomMarkupForUser_Installer {
                   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
                 ");
                 $db->query("ALTER TABLE `xf_3ps_cmfu_presets`
-                  ADD PRIMARY KEY (`preset_id`),
                   ADD UNIQUE KEY `preset_id` (`preset_id`);
                 ");
                 $db->query("ALTER TABLE `xf_data_registry` CHANGE `data_key` `data_key` VARBINARY(50) NOT NULL");
@@ -61,8 +61,9 @@ class ThreePointStudio_CustomMarkupForUser_Installer {
 
     public static final function uninstall() {
         $db = XenForo_Application::getDb();
-        $db->query("ALTER TABLE `xf_user`
-                    DROP COLUMN `3ps_cmfu_options`");
+        try {
+            $db->query("ALTER TABLE `xf_user` DROP COLUMN `3ps_cmfu_render_cache`");
+        } catch (Zend_Db_Statement_Mysqli_Exception $e) {}
         $db->query("DROP TABLE IF EXISTS `xf_3ps_cmfu_presets`");
         $db->query("ALTER TABLE `xf_data_registry` CHANGE `data_key` `data_key` VARBINARY(25) NOT NULL");
     }
